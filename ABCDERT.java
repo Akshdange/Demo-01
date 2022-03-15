c9c9684d2ed8a1f792091eb5cab6b153d7b694f1
Akshdange <101630944+Akshdange@users.noreply.github.com>
2022-03-15 17:15:11 +0530

    Add files via upload

---
+++ ABCDERT.java
@@ -0,0 +1,14 @@
+class ABCDERT{
+   public static void main(String ar[])
+       {  System.out.println("hello    students");
+
+          System.out.println("NAME:AKSHAY BALAJIRAO DANGE");
+
+          System.out.println("BRANCH: MECHANICAL ENGINEERING");
+
+          System.out.println("COLLEGE: ISB&M SOT PUNE");
+                                        
+
+
+
+                                              }}
