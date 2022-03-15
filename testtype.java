be80e356640619e57cdc08c7b04ae0f5a8605890
Akshdange <101630944+Akshdange@users.noreply.github.com>
2022-03-15 16:45:28 +0530

    Add files via upload

---
+++ testtype.java
@@ -0,0 +1,24 @@
+package pack;
+
+public class testtype { boolean b;
+int a;
+float c=90;
+double d;
+	public static void main(String[] args) 
+	{ 
+	  float r= 89.8f; // by default decimal is considered as double
+	  
+		
+	 testtype t= new testtype();
+	 System.out.println(t.b);
+	 System.out.println(t.a);
+	 System.out.println(t.c);
+	 System.out.println(t.d);
+	 
+	 
+	}
+
+}
+
+
+
